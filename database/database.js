dba22fb01c4f1a65a14feacb9d0cae68eee8c658
MOR-E <pedrohenrique88977944@gmail.com>
2023-06-22 08:44:35 -0300

    testebanco

--- database/database.js
+++ database/database.js
@@ -3,7 +3,12 @@ const Sequelize = require("sequelize");
 const connection = new Sequelize('railway','root', '3Rx23Trszd7KmSiJDG9q', {
     host: 'containers-us-west-158.railway.app',
     dialect: 'mysql',
-    timezone: "-03:00"
+    timezone: "-03:00",
+    port: 7600,
+    username: 'root',
+    password: '3Rx23Trszd7KmSiJDG9q',
+    database: 'railway',
+
 });
 
 module.exports = connection;
